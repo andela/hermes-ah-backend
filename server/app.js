e980f346165c259e0789408f3795b2c1e090348a
Amaechi Chuks <amaechichuks2000@yahoo.com>
2019-03-25 19:57:37 +0100

    ::chore(postgreSQL-setup):Setup postgresql database
    - setup postgres
    - setup sequelize
    - setup express
    
    [Finishes #164798004]

---
+++ server/app.js
@@ -0,0 +1,15 @@
+import '@babel/polyfill';
+import express from 'express';
+import cors from'cors';
+import db from './database/models/index';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 2000;
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App is listen on ${port}`);
+  db.sequelize.sync();
+});
